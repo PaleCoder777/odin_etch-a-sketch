6560a295e85344a8b815fbdec88e7a26b2226e12
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-13 19:28:45 -0400

    Add DOM variables for pixel section and pixels

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Project: Etch-a-Sketch //
 
 // DOM-Variables
+let pixelGridDOM = document.querySelector("#pixelGrid");
+let pixelDOM = document.querySelector(".pixel");
+console.log(pixelGridDOM);
+console.log(pixelDOM);
 
 // JS-Variables
 
