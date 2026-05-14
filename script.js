890a1e45b3b4c4a2cdf6bff07cd8ae7e4ca2cb65
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-14 19:14:38 -0400

    Reference new pixels and loop listeners
    Declared variable to reference all new pixels,
    turn into arraya from NodeList, and for of loop
    to give all of them event listeners.
    
    The event listered adds/gives classes on enter/
    leave.

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Project: Etch-a-Sketch //
 
 
-
+// Reference grid section + make grid
 let pixelGridDOM = document.querySelector("#pixelGrid");
 console.log(pixelGridDOM);
-
 addPixel();
+
+// Reference all pixels, turn to an array
+let pixelDOM = document.querySelectorAll(".pixel");
+pixelDOM = [...pixelDOM];
+
+// Loop array to give all pixels listeners
+// pixels change when mouse enter/leave
+for (let item of pixelDOM) {
+    item.addEventListener("mouseenter", () => {
+        item.classList.add("filled");
+        item.classList.add("inside");
+    });
+
+    item.addEventListener("mouseleave", () => {
+        item.classList.remove("inside");
+    });
+}
 
 
 
@@ -24,4 +40,4 @@ function addPixel() {
 
 // Project Considerations/Logic Errors:
 // (-) Hard coding the padding will only work for one scenario
-// 
+// (?) How to make event delegation work to avoid eventlistener spam?
