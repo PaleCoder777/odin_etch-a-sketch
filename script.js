a0cc5c140d89db7f680ce61f90182adf29818ede
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-15 08:35:14 -0400

    Add variables for determining squares and padding

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Project: Etch-a-Sketch //
 
+// Soft coding the padding style needed to fit in 500px max width
+const GRID_MAX_WIDTH = 500;
+const gridSideAmount = 16;
+const rowTotalBorderWidth = gridSideAmount * 2;
+const pixelPadding = (GRID_MAX_WIDTH - rowTotalBorderWidth) / (gridSideAmount);
+const pixelPaddingPerSide = pixelPadding / 2;
+console.log(pixelPaddingPerSide);
+
+// DOM Style specific, make it string + px
+let pixelPaddingDOM = (`${pixelPaddingPerSide}px`);
+console.log(pixelPaddingDOM);
+
+// Initial Grid Amount + declare a variable for new amount
+let totalSquares = gridSideAmount * gridSideAmount;
+console.log(gridSideAmount);
+console.log(totalSquares);
+let newgridSideAmount;
+let newTotalSquares;
+
+
 
 // Reference grid section + make grid
 let pixelGridDOM = document.querySelector("#pixelGrid");
@@ -9,6 +29,7 @@ addPixel();
 // Reference all pixels, turn to an array
 let pixelDOM = document.querySelectorAll(".pixel");
 pixelDOM = [...pixelDOM];
+console.log(pixelDOM);
 
 // Loop array to give all pixels listeners
 // pixels change when mouse enter/leave
@@ -26,17 +47,27 @@ for (let item of pixelDOM) {
 // Button prompt user for #, remove grid, add grid
 let newGridDOM = document.querySelector("#newGrid");
 newGridDOM.addEventListener("click", () => {
-    let newGridAmount = +prompt(`The new grid will have # of pixels on each side, please select this new #: \n (pick from range 16-100)`, 0);
+    newgridSideAmount = +prompt(`The new grid will have # of pixels on each side, please select this new #: \n (pick from range 16-100)`, 0);
+    console.log(newgridSideAmount);
+    newTotalSquares = (newgridSideAmount * newgridSideAmount);
+    console.log(newTotalSquares);
+    for (let pixel of pixelDOM) {
+        pixelGridDOM.removeChild(pixel);
+    }
+
 });
+
+
+
 
 
 
 // Functions
 function addPixel() {
-    
     for (let i = 1; i <= 256; i++) {
         let newPixelDOM = document.createElement("div");
         newPixelDOM.classList = ("pixel");
+        newPixelDOM.style.padding = pixelPaddingDOM;
         pixelGridDOM.appendChild(newPixelDOM);
     }
         
